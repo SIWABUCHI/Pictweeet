a2bcf99a3f2d5369eb8151d5e34b04ed0b1aaac0
SOTAIWABUCHI <sotaiwabuchi@gmail.com>
2019-03-07 20:21:51 +0900

    heroku

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,15 @@
 class ArticlesController < ApplicationController
 
   def index
+    @articles = Article.all
   end
 
   def new
   end
 
   def create
+    Article.create(title: params[:title], image: params[:image], content: params[:content])
+    redirect_to "/articles"
   end
 
   def destroy
